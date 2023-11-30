7bec957eb4bcd9405b27b25d9ee080b2298f7677
Work <Work@MSI>
2023-11-30 15:52:56 -0600

    Where we ended on Nov 30 in the latter class.

--- Chapter8-1436-002/Source.cpp
+++ Chapter8-1436-002/Source.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    
+    int theNumbers = 42;// 43;
+    //array ~ list 
+    //a = 43; 
+
+    vector <string> grocery_list =
+    {
+        "hard-boiled eggs",
+        "cheddar cheese",
+        "almonds"
+    };
+
+    //cout << grocery_list[1] << endl; 
+    grocery_list.push_back("pistachios");
+
+    //cout << grocery_list.size() << endl; 
+    for (int i = 0; i < grocery_list.size(); i++)
+    {
+        cout << grocery_list[i] << endl; 
+    }
+
     return 0;
 }
